bf521434f2598aaf791a0e016a9541144bd8a2ff
Rodrigo <guaxi.prog@gmail.com>
2024-10-04 16:16:25 -0300

    TernaryOperatorEx project creation

---
+++ TernaryOperatorEx/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    // Ternary Operator:
+    public static void main(String[] args) {
+        
+        int a = 6;
+        int b = 6;
+
+        String answer = a==b ? "true" : "false";
+
+        System.out.println(answer);
+    }
+}
